8ad7a20a63ca587b83946ed09a960ea62dd60d12
Aqila <aqilanabilaginanjar@gmail.com>
2026-06-11 08:24:29 +0700

    Add pra_uts folder

---
+++ pra_uts/bootstrap/app.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->alias([
+            'is_admin' => \App\Http\Middleware\IsAdmin::class,
+            'redirect_if_admin' => \App\Http\Middleware\RedirectIfAdmin::class,
+            'is_superadmin' => \App\Http\Middleware\IsSuperAdmin::class,
+        ]);
+
+        $middleware->validateCsrfTokens(except: [
+            'payment/callback',
+        ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions): void {
+        //
+    })->create();
